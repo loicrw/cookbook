312b2a87d15897ac255ccae4d147663b5f0a16f0
Loic RW <loicrw96@gmail.com>
2026-01-04 16:16:20 +0100

    Updated recipe types and icons

--- frontend/app/types/app.ts
+++ frontend/app/types/app.ts
@@ -1,5 +1,28 @@
-export interface AppData {
-  version: number;
+export type AppData = {
   count: number;
   lastUpdated: number;
-}
+  version: number;
+};
+
+export type Recipe = {
+  description: string;
+  ingredients: Ingredient[];
+  people_served: number;
+  steps: Step[];
+  times_cooked: number;
+  title: string;
+  author?: string;
+  last_cooked_on?: Date;
+  tags?: string[];
+};
+
+type Ingredient = {
+  amount: number | "to taste";
+  name: string;
+  unit?: string;
+};
+
+type Step = {
+  description: string;
+  order: number;
+};
